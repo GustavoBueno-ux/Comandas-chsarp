6be9b5b053d2021d7495b4a47310e2dfb4c984dc
Gustavo Bueno <ghggustavobcm@gmail.com>
2026-01-27 13:51:19 -0300

    Adicionando produto sobremesa

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ List<Produto> produtos = new List<Produto>()
     new Produto(3, "Gu Batatas", 9.90m),
     new Produto(4, "Gu Água", 3.00m),
     new Produto(5, "Gu Refri", 6.00m),
-    new Produto(6, "Gu Sorvete", 10.00m)
+    new Produto(6, "Gu Sorvete", 9.00m)
 };
 
 int Id = 1;
